2cfb5577e89bf0d40a14401be18c337105350f20
ZiglioNZ <emanuele@ziglioli.org>
2015-06-16 15:38:49 +1200

    Moved FirebugConsole classes to embedded, was having compilation errors

--- src/main/java/org/gogpsproject/GoGPS_Fx.java
+++ src/main/java/org/gogpsproject/GoGPS_Fx.java
@@ -1,14 +1,10 @@
 package org.gogpsproject;
 	
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.logging.Logger;
 
-import gnu.io.CommDriver;
-import gnu.io.CommPortIdentifier;
-
-import org.gogpsproject.model.FirebugConsoleError;
-import org.gogpsproject.model.FirebugConsoleInfo;
 import org.gogpsproject.model.GoGPSDef;
 import org.gogpsproject.model.GoGPSModel;
 import org.gogpsproject.model.SerialPortDef;
@@ -22,6 +18,37 @@ public class GoGPS_Fx {
   private static final Logger logger = Logger.getLogger(GoGPS_Fx.class.getName());
   static GoGPSModel goGPSModel;
 
+  public abstract static class FirebugConsole extends OutputStream {
+
+    abstract void log( String msg );
+
+    StringBuilder sb = new StringBuilder();
+    
+    @Override
+    public void write(int i) {
+      sb.append((char)i);
+    }
+
+    @Override
+    public void flush() {
+      if( sb.length() >0 && !sb.toString().equals("\r\n"))
+        log(sb.toString());
+      sb = new StringBuilder();
+    }  
+  }
+
+  public static class FirebugConsoleInfo extends FirebugConsole{
+    @net.java.html.js.JavaScriptBody(args = { "msg" }, body = ""
+        + "Firebug.Console.log(msg);")
+    public native void log( String msg );
+  }
+  
+  public static class FirebugConsoleError extends FirebugConsole{
+    @net.java.html.js.JavaScriptBody(args = { "msg" }, body = ""
+        + "Firebug.Console.error(msg);")
+    public native void log( String msg );
+  }
+  
   public static void main(String... args) throws Exception {
     System.out.println("Library Path is " + System.getProperty("java.library.path"));
     System.out.println("rootdir is " + System.getProperty("user.dir"));
@@ -43,6 +70,7 @@ public class GoGPS_Fx {
       }
       goGPSModel = new GoGPSModel();
       goGPSModel.getSpeedOptions().addAll( Arrays.asList(new Integer[]{9600, 115200}));
+      goGPSModel.getMeasurementRateOptions().addAll( Arrays.asList(new Integer[]{1, 2, 5, 10}));
       Models.toRaw(goGPSModel);
       GoGPSDef.registerModel();
       goGPSModel.applyBindings();
