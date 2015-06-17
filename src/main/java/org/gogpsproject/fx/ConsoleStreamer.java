cfc1df823f211866a79030df3b660c1903f234c5
ZiglioNZ <emanuele@ziglioli.org>
2015-06-18 00:41:00 +1200

    deleted

--- src/main/java/org/gogpsproject/fx/ConsoleStreamer.java
+++
@@ -1,8 +0,0 @@
-package org.gogpsproject.fx;
-
-import org.gogpsproject.Coordinates;
-import org.gogpsproject.EphGps;
-import org.gogpsproject.IonoGps;
-import org.gogpsproject.Observations;
-import org.gogpsproject.StreamEventListener;
-
