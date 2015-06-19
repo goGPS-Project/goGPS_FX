e6725cf2c6816e7909bb084ee5e4497ec8599066
ZiglioNZ <emanuele@ziglioli.org>
2015-06-19 23:14:40 +1200

    Experimented with callback, didn't work (see index.html instead)

--- src/main/resources/org/gogpsproject/fx/goGPS.js
+++ src/main/resources/org/gogpsproject/fx/goGPS.js
@@ -1,11 +1,15 @@
-//console.log("goGPS.js");
+console.log("goGPS.js");
 if (!document.getElementById('FirebugLite')){
 	E = document['createElement' + 'NS'] && document.documentElement.namespaceURI;
 	E = E ? document['createElement' + 'NS'](E, 'script') : document['createElement']('script');
 	E['setAttribute']('id', 'FirebugLite');
-	E['setAttribute']('src', 'http://getfirebug.com/releases/lite/latest/firebug-lite-debug.js');
+//	E['setAttribute']('src', 'http://getfirebug.com/releases/lite/latest/firebug-lite-debug.js');
+	E['setAttribute']('src', 'js/firebug-lite/firebug-lite-debug.js');
 	E['setAttribute']('FirebugLite', '4');
 	E.onload = function() {
+		  console.log(Firebug);
+		  console.log(Firebug.chrome);
+		  console.log(Firebug.chrome.toggle);
 		  Firebug.chrome.toggle();
      };
 	(document['getElementsByTagName']('head')[0] || document['getElementsByTagName']('body')[0]).appendChild(E);
